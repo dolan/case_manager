cbbbeea6efe7c481d96ad5b7a63f6a2efdc4f829
Dave Dolan <ddolan@decisiv.com>
2024-12-04 23:20:17 -0500

    [Remove debugging logging]
    Remove debug logging

--- app/controllers/concerns/authenticatable.rb
+++ app/controllers/concerns/authenticatable.rb
@@ -10,12 +10,9 @@ module Authenticatable
 
   def set_current_user
     @current_user = request.env["current_user"]
-    Rails.logger.debug("Setting current user in Authenticatable: #{@current_user.inspect}")
-    Rails.logger.debug("Current user class: #{@current_user.class}") if @current_user
   end
 
   def current_user
-    Rails.logger.debug("Getting current user in Authenticatable: #{@current_user.inspect}")
     @current_user
   end
 end
--- app/middleware/api_authentication.rb
+++ app/middleware/api_authentication.rb
@@ -8,13 +8,10 @@ class ApiAuthentication
     request = ActionDispatch::Request.new(env)
     auth_header = request.headers["Authorization"]
 
-    Rails.logger.debug("API Authentication: #{auth_header}")
 
     if auth_header&.start_with?("Bearer ")
       token = auth_header.split(" ").last
       if user = authenticate_request(token)
-        Rails.logger.debug("Setting user in middleware: #{user.inspect}")
-        Rails.logger.debug("User class: #{user.class}")
         env["current_user"] = user
         @app.call(env)
       else
@@ -28,9 +25,7 @@ class ApiAuthentication
   private
 
   def authenticate_request(token)
-    Rails.logger.debug("Authenticating request with token: #{token}")
     api_key = ApiKey.find_by(key: token)
-    Rails.logger.debug("API key: #{api_key.inspect}")
     api_key&.user
   end
 end
