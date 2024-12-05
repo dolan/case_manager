88fb5a4153c43baaa740a7ed4d5ce59ea1396459
Dave Dolan <ddolan@decisiv.com>
2024-12-04 21:54:40 -0500

    [Businesses Controller is working]
    Businesses controller is fully working with the permissions system
    This controller is meant to be the template for the rest of the
    endpoints

--- app/controllers/api/v1/businesses_controller.rb
+++ app/controllers/api/v1/businesses_controller.rb
@@ -2,7 +2,6 @@ module Api
   module V1
     class BusinessesController < Api::BaseController
       def index
-        Rails.logger.debug("User before authorize!: #{current_user.inspect}")
         authorize! Business, to: :index?
         businesses = Business.all
         render json: ::V1::BusinessResource.new(businesses).serialize
@@ -14,7 +13,8 @@ module Api
       end
 
       def create
-        business = authorize!(::Business.new(business_params))
+        business = ::Business.new(business_params.merge(created_by_user_id: current_user.id))
+        authorize! business, to: :create?
 
         if business.save
           render json: ::V1::BusinessResource.new(business).serialize, status: :created
@@ -24,7 +24,8 @@ module Api
       end
 
       def update
-        business = authorize!(::Business.find_by!(uuid: params[:id]))
+        business = ::Business.find_by!(uuid: params[:id])
+        authorize! business, to: :update?
 
         if business.update(business_params)
           render json: ::V1::BusinessResource.new(business).serialize
@@ -34,7 +35,8 @@ module Api
       end
 
       def destroy
-        business = authorize!(::Business.find_by!(uuid: params[:id]))
+        business = ::Business.find_by!(uuid: params[:id])
+        authorize! business, to: :destroy?
         business.destroy
 
         head :no_content
@@ -43,7 +45,7 @@ module Api
       private
 
       def business_params
-        params.require(:business).permit(:name)
+        params.require(:business).permit(:name, :description, :category, :brand_identifier, :metadata)
       end
     end
   end
--- app/resources/v1/business_resource.rb
+++ app/resources/v1/business_resource.rb
@@ -7,7 +7,5 @@ module V1
     attributes :uuid, :name, :created_at, :updated_at
 
     many :business_locations, resource: "V1::BusinessLocationResource", id_attribute: :uuid
-    many :cases, resource: "V1::CaseResource", id_attribute: :uuid
-    many :assets, resource: "V1::AssetResource", id_attribute: :uuid
   end
 end
